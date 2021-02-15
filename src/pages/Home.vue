ef010d04d0af1ada1caf454ad7f974284dc1eb3a
Alex Vieira <alex.p.vieira@gmail.com>
2021-02-15 10:16:25 -0300

    Changing home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,7 @@
         <div class="row q-col-gutter-md">
             <div class="col-xs-12 col-sm-4 col-lg-3" v-for="(card, index) in cards" :key="index">
                 <q-card class="text-center q-py-lg bg-secondary text-white">
-                    <q-card-section class="text-h2 text-weight-bold">
+                    <q-card-section class="text-h3 text-weight-bold">
                         <i-count-up :start="0" :endVal="card.number" :duration="5" :options="options"></i-count-up>
                     </q-card-section>
 
